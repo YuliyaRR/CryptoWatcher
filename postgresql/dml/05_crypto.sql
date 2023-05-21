192ef8516d8c162e4d6281a41a927449ab7de0c4
Yuliya Romashko <ramashkay@gmail.com>
2023-05-22 00:35:08 +0300

    Created: dml(postgresql) for DB

---
+++ postgresql/dml/05_crypto.sql
@@ -0,0 +1,8 @@
+INSERT INTO app.crypto(id, symbol, current_rate)
+VALUES (90, 'BTC', 0.0);
+
+INSERT INTO app.crypto(id, symbol, current_rate)
+VALUES (80, 'ETH', 0.0);
+
+INSERT INTO app.crypto(id, symbol, current_rate)
+VALUES (48543, 'SOL', 0.0);
