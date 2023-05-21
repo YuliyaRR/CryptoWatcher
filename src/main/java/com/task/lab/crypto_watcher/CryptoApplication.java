ef1832232d9979e503410e70dff31ede03a5a867
Yuliya Romashko <ramashkay@gmail.com>
2023-05-22 00:07:11 +0300

    initial settings

---
+++ src/main/java/com/task/lab/crypto_watcher/CryptoApplication.java
@@ -0,0 +1,13 @@
+package com.task.lab.crypto_watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApplication.class, args);
+	}
+
+}
